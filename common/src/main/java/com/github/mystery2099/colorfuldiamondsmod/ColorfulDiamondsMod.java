32e198e06df3a2a765647baf04f1323e056c8959
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-12-16 16:39:15 -0500

    Created enum for armor materials and registered a set of armor items for each armor material. Also Replaced block and item creative tabs with default and combat item groups

--- common/src/main/java/com/github/mystery2099/colorfuldiamondsmod/ColorfulDiamondsMod.java
+++ common/src/main/java/com/github/mystery2099/colorfuldiamondsmod/ColorfulDiamondsMod.java
@@ -1,5 +1,6 @@
 package com.github.mystery2099.colorfuldiamondsmod;
 
+import com.github.mystery2099.colorfuldiamondsmod.item.ModItems;
 import dev.architectury.registry.CreativeTabRegistry;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -7,12 +8,13 @@ import net.minecraft.util.Identifier;
 
 public class ColorfulDiamondsMod {
     public static final String MOD_ID = "colorful_diamonds_mod";
-    public static final ItemGroup BLOCK_TAB = CreativeTabRegistry.create(new Identifier(ColorfulDiamondsMod.MOD_ID, "blocks"), () ->
-            new ItemStack(ModBlocks.WHITE_DIAMOND_BLOCK.get().asItem()));
-    public static final ItemGroup ITEM_TAB = CreativeTabRegistry.create(new Identifier(ColorfulDiamondsMod.MOD_ID, "items"), () ->
-            new ItemStack(ModItems.WHITE_DIAMOND.get().asItem()));
+    public static final ItemGroup DEFAULT_ITEM_GROUP = CreativeTabRegistry.create(new Identifier(ColorfulDiamondsMod.MOD_ID, "default"), () ->
+            new ItemStack(ModItems.WHITE_DIAMOND.get()));
+    public static final ItemGroup COMBAT_ITEM_GROUP = CreativeTabRegistry.create(new Identifier(ColorfulDiamondsMod.MOD_ID, "combat"), () ->
+            new ItemStack(ModItems.ARMOR.get(0).get()));
     public static void init() {
         ModBlocks.BLOCKS.register();
         ModItems.ITEMS.register();
+
     }
 }
