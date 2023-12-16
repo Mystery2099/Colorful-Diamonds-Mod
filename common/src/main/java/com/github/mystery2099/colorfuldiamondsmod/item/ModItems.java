b435038a232ddf845c869187a73d5158d032446d
Mystery2099 <xxmysteryxxftb@gmail.com>
2023-12-16 17:21:15 -0500

    changed grouping of recipes and removed any use for forge tags in favor of explicit minecraft items

--- common/src/main/java/com/github/mystery2099/colorfuldiamondsmod/item/ModItems.java
+++ common/src/main/java/com/github/mystery2099/colorfuldiamondsmod/item/ModItems.java
@@ -17,24 +17,6 @@ import java.util.function.Supplier;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ColorfulDiamondsMod.MOD_ID, Registry.ITEM_KEY);
-    /*public static final RegistrySupplier<Item> WHITE_DIAMOND = gem(DyeColor.WHITE);
-    public static final RegistrySupplier<Item> ORANGE_DIAMOND = gem(DyeColor.ORANGE);
-    public static final RegistrySupplier<Item> MAGENTA_DIAMOND = gem(DyeColor.MAGENTA);
-    public static final RegistrySupplier<Item> LIGHT_BLUE_DIAMOND = gem(DyeColor.LIGHT_BLUE);
-    public static final RegistrySupplier<Item> YELLOW_DIAMOND = gem(DyeColor.YELLOW);
-    public static final RegistrySupplier<Item> LIME_DIAMOND = gem(DyeColor.LIME);
-    public static final RegistrySupplier<Item> PINK_DIAMOND = gem(DyeColor.PINK);
-    public static final RegistrySupplier<Item> GRAY_DIAMOND = gem(DyeColor.GRAY);
-    public static final RegistrySupplier<Item> LIGHT_GRAY_DIAMOND = gem(DyeColor.LIGHT_GRAY);
-    public static final RegistrySupplier<Item> CYAN_DIAMOND = gem(DyeColor.CYAN);
-    public static final RegistrySupplier<Item> PURPLE_DIAMOND = gem(DyeColor.PURPLE);
-    public static final RegistrySupplier<Item> BLUE_DIAMOND = gem(DyeColor.BLUE);
-    public static final RegistrySupplier<Item> BROWN_DIAMOND = gem(DyeColor.BROWN);
-    public static final RegistrySupplier<Item> GREEN_DIAMOND = gem(DyeColor.GREEN);
-    public static final RegistrySupplier<Item> RED_DIAMOND = gem(DyeColor.RED);
-    public static final RegistrySupplier<Item> BLACK_DIAMOND = gem(DyeColor.BLACK);*/
-
-
     public static final List<RegistrySupplier<Item>> GEMS = new ArrayList<>();
     public static final List<RegistrySupplier<Item>> ARMOR = new ArrayList<>();
     static {
